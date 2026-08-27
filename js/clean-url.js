d3a52fb559b021870f0e12a98f6d22ab1ecbf8f5
GreenTailedCat <151253915+GreenTailedCat@users.noreply.github.com>
2026-08-27 16:36:36 -0300

    Adicionar Monetag In-Page Push

--- js/clean-url.js
+++ js/clean-url.js
@@ -1,1 +1,4 @@
 // Ambiente de teste: redirecionamentos legados desativados.
+
+// Monetag In-Page Push
+(function(s){s.dataset.zone='11670365',s.src='https://nap5k.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))
